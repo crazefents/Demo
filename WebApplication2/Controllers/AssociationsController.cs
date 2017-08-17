d9f00e11aae39c4adc7738378cf75c92abf122da
crazefents <kefentsecrocomathibe@gmail.com>
2017-08-17 15:54:09 +0200

    popover
    sarsdtger

---
+++ WebApplication2/Controllers/AssociationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AssociationsController : Controller
+    {
+        private SubsidiariesEntities db = new SubsidiariesEntities();
+
+        // GET: Associations
+        public ActionResult Index()
+        {
+            var associations = db.Associations.Include(a => a.AssociationType);
+            return View(associations.ToList());
+        }
+
+        // GET: Associations/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Association association = db.Associations.Find(id);
+            if (association == null)
+            {
+                return HttpNotFound();
+            }
+            return View(association);
+        }
+
+        // GET: Associations/Create
+        public ActionResult Create()
+        {
+            ViewBag.AssocType = new SelectList(db.AssociationTypes, "AssocType", "Description");
+            return View();
+        }
+
+        // POST: Associations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompanyID,AssocID,AssocType,AssPercent,DirectRel,UpdateDate,ChangeDate,NoShares_YN")] Association association)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Associations.Add(association);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AssocType = new SelectList(db.AssociationTypes, "AssocType", "Description", association.AssocType);
+            return View(association);
+        }
+
+        // GET: Associations/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Association association = db.Associations.Find(id);
+            if (association == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AssocType = new SelectList(db.AssociationTypes, "AssocType", "Description", association.AssocType);
+            return View(association);
+        }
+
+        // POST: Associations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompanyID,AssocID,AssocType,AssPercent,DirectRel,UpdateDate,ChangeDate,NoShares_YN")] Association association)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(association).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AssocType = new SelectList(db.AssociationTypes, "AssocType", "Description", association.AssocType);
+            return View(association);
+        }
+
+        // GET: Associations/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Association association = db.Associations.Find(id);
+            if (association == null)
+            {
+                return HttpNotFound();
+            }
+            return View(association);
+        }
+
+        // POST: Associations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Association association = db.Associations.Find(id);
+            db.Associations.Remove(association);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
