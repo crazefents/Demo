ddef6de7fec6fac22404c0065bdae4a172d2b3ce
crazefents <kefentsecrocomathibe@gmail.com>
2017-08-18 15:55:52 +0200

    the database

--- WebApplication2/Controllers/CompanyNamesController.cs
+++ WebApplication2/Controllers/CompanyNamesController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -17,46 +15,53 @@ namespace WebApplication2.Controllers
         private SubsidiariesEntities db = new SubsidiariesEntities();
 
         // GET: CompanyNames
-        public ActionResult Index2(string CompanyTypeID, string ExchangeCode, string searchString, string fields)
+        public ActionResult Index2(string CountryID, string BusinessSectorID, string CompanyName1, string fields, string CompanyTypeID, string ExchangeCode, string searchString, string ExchangeCodeName)
         {
 
             // string searchby = "Company Name" + "Short Code" + "CountryID" + "BusinessSectorID";
-            //var field = new List<string>();
+
+            //  var field = new List<string>();
             var allList = new object[] { "Company Names", "Country", "Business Sector" };
-            ViewBag.fields = new SelectList(allList);
+
+            var both = db.Exchanges.OrderBy(q => q.CountryID).ToDictionary(q => q.ExchangeName, q => q.ExchangeCode + " " + q.ExchangeName);
+            ViewBag.ExchangeCodeName = new SelectList(both, "Key", "Value");
+
 
             var GenreLst = new List<string>();
 
-            var GenreQry = from d in db.CompanyNames
-                           orderby d.ExchangeCode
-                           select d.ExchangeCode;
+            //var GenreQry = from d in db.CompanyNames
+            //               orderby d.ExchangeCode
+            //               select d.ExchangeCode;
 
-            GenreLst.AddRange(GenreQry.Distinct());
+            //   GenreLst.AddRange(GenreQry.Distinct());
             ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName");
-            ////////////////////////////////////////
 
-            var TypLst = new List<string>();
 
-            var TypQry = from d in db.CompanyNames
-                         orderby d.CompanyTypeID
-                         select d.CompanyTypeID;
+            // var TypLst = new List<string>();
 
-            TypLst.AddRange(TypQry.Distinct());
+            //var TypQry = from d in db.CompanyNames
+            //             orderby d.CompanyTypeID
+            //             select d.CompanyTypeID;
+
+            // TypLst.AddRange(TypQry.Distinct());
             ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc");
 
-            /////////////////////////////////////////
-           
+
+
+            ViewBag.fields = new SelectList(allList);
 
 
             var companyNames = from m in db.CompanyNames
                                select m;
 
-            /////////////////////////////////////
+
+
 
             if (!String.IsNullOrEmpty(searchString))
             {
 
- 
+
+
                 if (fields.Equals("Company Names"))
                 {
                     companyNames = companyNames.Where(s => s.CompanyName1.Contains(searchString));
@@ -79,6 +84,7 @@ namespace WebApplication2.Controllers
 
 
 
+
             if (!string.IsNullOrEmpty(ExchangeCode))
             {
                 companyNames = companyNames.Where(x => x.ExchangeCode == ExchangeCode);
@@ -91,6 +97,7 @@ namespace WebApplication2.Controllers
 
             return View(companyNames);
         }
+
 
         // GET: CompanyNames/Details/5
         public ActionResult Details(int? id)
@@ -110,29 +117,27 @@ namespace WebApplication2.Controllers
         // GET: CompanyNames/Create
         public ActionResult Create()
         {
-            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName");
             ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc");
+            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName");
             return View();
         }
 
-        // POST: CompanyNames1/Create
+        // POST: CompanyNames/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public ActionResult Create([Bind(Include = "CompanyID,ExchangeCode,CompanyName1,ShortCode,CorpInfo,CountryID,BusinessSectorID,CompanyTypeID,UpdateDate")] CompanyName companyName)
         {
             if (ModelState.IsValid)
             {
-                companyName.UpdateDate = DateTime.Now;
                 db.CompanyNames.Add(companyName);
                 db.SaveChanges();
-                return RedirectToAction("Index2");
+                return RedirectToAction("Index");
             }
 
-            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
             ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc", companyName.CompanyTypeID);
+            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
             return View(companyName);
         }
 
@@ -148,8 +153,8 @@ namespace WebApplication2.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
             ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc", companyName.CompanyTypeID);
+            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
             return View(companyName);
         }
 
@@ -162,22 +167,17 @@ namespace WebApplication2.Controllers
         {
             if (ModelState.IsValid)
             {
-                
-                    //db.Set<CompanyName>().AddOrUpdate(companyName);
-                    //db.SaveChanges();
-                    db.Entry(companyName).State = EntityState.Modified;
-                    db.SaveChanges();
-                
-            return RedirectToAction("Index2");
-        }
-        ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
-        ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc", companyName.CompanyTypeID);
+                db.Entry(companyName).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CompanyTypeID = new SelectList(db.CompanyTypes, "CompanyTypeID", "CompanyTypeDesc", companyName.CompanyTypeID);
+            ViewBag.ExchangeCode = new SelectList(db.Exchanges, "ExchangeCode", "ExchangeName", companyName.ExchangeCode);
             return View(companyName);
-    }
+        }
 
-
-    // GET: CompanyNames/Delete/5
-    public ActionResult Delete(int? id)
+        // GET: CompanyNames/Delete/5
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
@@ -199,7 +199,7 @@ namespace WebApplication2.Controllers
             CompanyName companyName = db.CompanyNames.Find(id);
             db.CompanyNames.Remove(companyName);
             db.SaveChanges();
-            return RedirectToAction("Index2");
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
